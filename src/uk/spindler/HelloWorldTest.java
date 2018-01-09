c44061c66e79315a799e6a09c9b4b23cbc86208b
Matt Spindler <matthew@spindler.uk>
2018-01-09 17:37:11 +0000

    Add test file

---
+++ src/uk/spindler/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package uk.spindler;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest extends HelloWorld {
+
+	@Test
+	public void test() {
+		int x=1;
+		assertTrue(x==1);
+	}
+
+}
